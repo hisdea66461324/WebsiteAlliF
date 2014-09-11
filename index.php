c016b93cbe0fa1a8f5ba483142c21781e0f867ca
HISDEA <hisdea@me.com>
2014-09-11 21:03:30 +0900

    app engine 서버에 있던 소스 옮기기

--- index.php
+++ index.php
@@ -1,1 +1,62 @@
-test
+<!DOCTYPE html>
+<html>
+<head>
+<title>ALL If - Homepage</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width" data-keepAlive="true"/>
+<style>
+body {
+	background-color: #f3f5f4;
+	margin:0;
+	padding: 0;
+	width:100%;
+	height:100%;
+	text-align:center;
+}
+
+
+#middleSection {
+	background-color: #dd3430;
+	padding:0 0 0 0;
+	margin: 0 0 0 0;
+	text-align:center;
+}
+
+#bottomSection {
+	background-color: #f3f5f4;	
+	text-align:center;
+}
+
+
+
+</style>
+</head>
+<body>
+<div id="middleSection">
+	<img src="/images/mainContent.gif"/>
+</div>	
+<div id="bottomSection">
+	<table cellpadding="0" cellspacing="0" border="0" width="1113" align=center>
+		<tr>
+			<td>
+				<img src="/images/bottom_left.jpg" id="bottomImg1"/>
+			</td>
+			<td width=500>&nbsp;
+			<a href="/contact/index.php"><input type="button" value="contact"></a>
+			</td>
+			<td>
+				<img src="/images/bottom_right.jpg" id="bottomImg2"/>
+			</td>
+		</tr>
+	</table>		
+</div>	
+</body>
+</html>
+	
+
+
+
+
+
+
+
